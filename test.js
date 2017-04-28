a734255a6ffa425c15bb080201c6b3337b6deaf8
Andre Caetano <webandrecaetano@gmail.com>
2017-04-28 12:43:11 -0300

    test

--- test.js
+++ test.js
@@ -42,7 +42,8 @@ test.cb('should resize image with percent option',function(t){
 	},function(err,data){
 		var outputFiles = glob.sync(dest+'/*');
 		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+		console.log(outputFiles)
+		// var outputImageSize = sizeOf(outputFiles[0]);
 
 		t.falsy(err);
 		t.truthy(numOutput==numOfInput);
@@ -53,182 +54,182 @@ test.cb('should resize image with percent option',function(t){
 	})
 });
 
-test.cb('should resize image with only one percent dimension',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test3'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with only one percent dimension',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test3'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		width:'50%'
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		width:'50%'
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==inputImageSize.height);
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==inputImageSize.height);
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with both px dimensions',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test4'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with both px dimensions',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test4'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		width:100,
-		height:100,
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		width:100,
+// 		height:100,
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==100);
-		t.truthy(outputImageSize.height==100);
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==100);
+// 		t.truthy(outputImageSize.height==100);
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with both dimensions but one is percent',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test5'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with both dimensions but one is percent',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test5'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		width:'50%',
-		height:100,
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		width:'50%',
+// 		height:100,
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==100);
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==100);
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with both dimensions but one auto',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test6'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with both dimensions but one auto',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test6'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		width:'50%',
-		height:'auto',
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		width:'50%',
+// 		height:'auto',
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==Math.floor(outputImageSize.width*inputImageSize.height/inputImageSize.width));
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==Math.floor(outputImageSize.width*inputImageSize.height/inputImageSize.width));
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with percent option as number',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test7'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with percent option as number',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test7'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		porcent:50
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		porcent:50
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with percent option as string without %',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test8'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with percent option as string without %',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test8'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		porcent:'50'
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		porcent:'50'
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
-test.cb('should resize image with percent and algo',function(t){
-	var src = './test/wizz/*.png';
-	var dest = './test/dist/test9'
-	var inputFiles = glob.sync(src);
-	var numOfInput = inputFiles.length;
-	var inputImageSize = sizeOf(inputFiles[0]);
+// test.cb('should resize image with percent and algo',function(t){
+// 	var src = './test/wizz/*.png';
+// 	var dest = './test/dist/test9'
+// 	var inputFiles = glob.sync(src);
+// 	var numOfInput = inputFiles.length;
+// 	var inputImageSize = sizeOf(inputFiles[0]);
 
-	del.sync(dest);
+// 	del.sync(dest);
 
-	self(src,dest,{
-		porcent:'50%',
-		algo:'bicubic',
-	},function(err,data){
-		var outputFiles = glob.sync(dest+'/*');
-		var numOutput = outputFiles.length;
-		var outputImageSize = sizeOf(outputFiles[0]);
+// 	self(src,dest,{
+// 		porcent:'50%',
+// 		algo:'bicubic',
+// 	},function(err,data){
+// 		var outputFiles = glob.sync(dest+'/*');
+// 		var numOutput = outputFiles.length;
+// 		var outputImageSize = sizeOf(outputFiles[0]);
 
-		t.falsy(err);
-		t.truthy(numOutput==numOfInput);
-		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
-		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
-		t.pass();
-		t.end();
-	})
-});
+// 		t.falsy(err);
+// 		t.truthy(numOutput==numOfInput);
+// 		t.truthy(outputImageSize.width==Math.floor(inputImageSize.width*(50/100)));
+// 		t.truthy(outputImageSize.height==Math.floor(inputImageSize.height*(50/100)));
+// 		t.pass();
+// 		t.end();
+// 	})
+// });
 
